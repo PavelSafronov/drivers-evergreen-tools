ed37de99fe1d49d1669a64e799b446589adc9608
Pavel Safronov <pavel.safronov@gmail.com>
2025-12-23 13:22:25 -0800

    add a debug call

--- .evergreen/auth_aws/aws_setup.sh
+++ .evergreen/auth_aws/aws_setup.sh
@@ -35,6 +35,8 @@ if [ -f $SCRIPT_DIR/test-env.sh ]; then
     rm $SCRIPT_DIR/test-env.sh
 fi
 
+echo "MongoDB_URI before aws_tester.py: ${MONGODB_URI:-}"
+
 export PROJECT_DIRECTORY
 python aws_tester.py "$@"
 
